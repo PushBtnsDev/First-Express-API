d6e68000142130d95867d58f7ec80b1bb1c61c5e
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-08 18:27:27 +1100

    routes

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`listening on Port: ${PORT}`))
 
 mongoose.connect('mongodb://localhost:27017/sample_mflix', {useUnifiedTopology: true,  useNewUrlParser: true}, (err, success) => {
   if (err) { return console.error(err) }
   console.log('Connection Status: Success');
 });
+
+// Import Routes
+
+const commentRoutes = require('./routes/comments');
+const userRoutes = require('./routes/users');
+
+app.use('/comments', commentRoutes);
+app.use('/users', userRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on Port: ${PORT}`))
