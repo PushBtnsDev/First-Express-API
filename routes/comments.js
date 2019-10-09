11aacd914644a28add6d8d8e89d8a46311485db6
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-09 12:11:51 +1100

    comment and user model setup

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Comment = require('./../models/Comment');
 
-router.get('/', (req, res, next) => {
-  res.send('Successful Req to /comments');
+router.get('/', async (req, res, next) => {
+  try {
+    const comments = await Comment.find().limit(20);
+    res.send(comments);
+  }
+  catch(err) {
+    res.status(500).send(err)
+  }
 });
 
 module.exports = router;
