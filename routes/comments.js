0eec861223600a55ed4915a2f602e6bdfda226d9
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-10 13:52:40 +1100

    get users and get user by id

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Comment = require('./../models/Comment');
+const Comment = require('../models/Comment');
 
 router.get('/', async (req, res, next) => {
   // GET all comments
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/User')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // GET all users
+  try {
+    const users = await User.find().limit(20);
+    res.send(users);
+  }
+  catch(err) {
+    res.status(500).send(err);
+  }
 });
+
+router.get('/:id', async (req, res, next) => {
+  // GET one user by id
+  try {
+    const user = await User.findById(req.params.id, (err, user) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(user);
+      }
+    })
+  }
+  catch(err) {
+    res.status(500).send(err);
+  }
+})
 
 module.exports = router;
